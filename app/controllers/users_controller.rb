e085d3aed9f1e4d42c24bce033c46033b998fcb5
someotherkyle <kylemclellan@gmail.com>
2019-06-04 08:13:23 -0400

    user new / create mostly complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,22 @@ class UsersController < ApplicationController
   end
 
   def create
-    if user = User.create(user_params)
-      session[:user_id] = user.id
-      redirect_to pantry(new)
-    else
-      render :new
+    if params[:user][:password] == params[:user][:password_confirmation]
+      if user = User.create(user_params)
+        session[:user_id] = user.id
+        redirect_to user
+      else
+        render :new
+      end
     end
   end
+
+  def show
+    @user = User.find_by(id: session[:user_id])
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
 end
