ee7a4a08c1e3b3438441015a2fa47bc62ad07809
someotherkyle <kylemclellan@gmail.com>
2019-06-04 17:21:05 -0400

    added relationships to models

--- app/models/pantry.rb
+++ app/models/pantry.rb
@@ -1,2 +1,5 @@
 class Pantry < ApplicationRecord
+  belongs_to :user
+  has_many :available_ingredients
+  has_many :ingredients, through: :available_ingredients
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,4 @@
 class Recipe < ApplicationRecord
+  has_many :required_ingredients
+  has_many :ingredients, through: :required_ingredients
 end
