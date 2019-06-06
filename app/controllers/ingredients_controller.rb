94a8a5e23023b6421e6259e215a155337f179d5b
someotherkyle <kylemclellan@gmail.com>
2019-06-06 10:07:27 -0400

    added create ingredient functionality

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,14 @@
+class IngredientsController < ApplicationController
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.create(ingredient_params)
+  end
+
+  private
+    def ingredient_params
+      params.require(:ingredient).permit(:name, :wet)
+    end
+end
