cc557405cdfdfeae81a332a03a8a62c8b93d7fa7
someotherkyle <kylemclellan@gmail.com>
2019-06-06 10:00:21 -0400

    new pantry functionality established

--- app/controllers/pantries_controller.rb
+++ app/controllers/pantries_controller.rb
@@ -1,12 +1,24 @@
 class PantriesController < ApplicationController
   before_action :require_login
   def new
-    @ingredients = Ingredient.all
     @pantry = Pantry.new
+  end
+
+  def edit
+    @dry = ['teaspoon', 'tablespoon', 'cup']
+    @wet = ['fluid ounce', 'cup', 'pint', 'quart', 'gallon']
+    @ingredients = Ingredient.all
   end
 
   def create
     pantry = Pantry.create(pantry_params)
+    pantry.user_id = current_user.id
+    pantry.save
+    redirect_to pantry_path(pantry)
+  end
+
+  def show
+    @pantry = Pantry.find_by(id: params[:id])
   end
 
   private
